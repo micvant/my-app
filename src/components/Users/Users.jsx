14b514d9c919f87060c04b404b2ec43ecfc9a639
Anton <33568973+micvant@users.noreply.github.com>
2022-03-02 14:27:06 +0900

    49. end lessons

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import './Users.module.css';
 
+
 let Users = (props) => {
 
-    let arrayUsers = props.users.map(u => <div key={u.id}>{u.fullName}</div>);
+    let arrayUsers = props.users.map(u => <div key={u.id}>
+        <span>
+            <div><img src={u.pathImg}/></div>
+            <div>{ u.followed ? <button onClick={() => props.changeUnFollowedUser(u.id)}>unfollow</button> : <button onClick={() => props.changeFollowedUser(u.id)}>follow</button>}</div>
+        </span>
+        <span>
+            <span>
+                <div>{u.fullName}</div>
+                <div>{u.status}</div>
+            </span>
+            <span>
+                <div>{u.location.city}</div>
+                <div>{u.location.country}</div>
+            </span>
+        </span>
+    </div>);
     return (
         <div>
             {arrayUsers}
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {followAC, unfollowAC} from '../../Data/users-reducer';
+import {followAC, setUsersAC, unfollowAC} from '../../Data/users-reducer';
 
 let  mapStateToProps = (state) => {
     return {
@@ -11,11 +11,15 @@ let  mapStateToProps = (state) => {
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        changeFollowedUser : () => {
-            let action = followAC();
+        changeFollowedUser : (userID) => {
+            let action = followAC(userID);
             dispatch(action);},
-        changeUnFollowedUser : () => {
-            let action = unfollowAC();
+        changeUnFollowedUser : (userID) => {
+            let action = unfollowAC(userID);
+            dispatch(action);
+        },
+        setUsers : (users) => {
+            let action = setUsersAC(users);
             dispatch(action);
         }
     };

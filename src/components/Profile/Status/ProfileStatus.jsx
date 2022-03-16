f14ef8052a2c3b2fe340c198a95e1b5ea67c61e2
Anton <33568973+micvant@users.noreply.github.com>
2022-03-16 09:30:59 +0900

    71. add use hook useState

--- src/components/Profile/Status/ProfileStatus.jsx
+++ src/components/Profile/Status/ProfileStatus.jsx
@@ -1,9 +1,11 @@
+import {useState} from "react";
 
 
 let ProfileStatus = (props) =>{
+    const [editMode, setEditMode] = useState(false);
  return (
     <>
-        {props.status !== null ? <div><span>{props.status}</span></div> : <div><input value={props.status}/></div>
+        {editMode === false ? <div><span onClick={()=> setEditMode(true)}>{props.status}</span></div> : <div><input value={props.status}/></div>
         }
     </>
  )
